e8b84fb3e8df875f51b78e6c4407de66bed57496
Diego Navarro Mellén <dnavarro@qdqmedia.com>
2013-10-30 15:43:21 +0100

    Trying to get travis working

--- setup.py
+++ setup.py
@@ -48,7 +48,6 @@ setup(
         'Topic :: Scientific/Engineering :: Image Recognition',
         'Topic :: Utilities',
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
