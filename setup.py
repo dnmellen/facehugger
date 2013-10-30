3e1935b1cf24da507de1bc16a3dd3162e698cded
Diego Navarro Mellén <dnavarro@qdqmedia.com>
2013-10-30 16:01:44 +0100

    Removing support for python 3.3 (SimpleCV don't support it)

--- setup.py
+++ setup.py
@@ -49,8 +49,6 @@ setup(
         'Topic :: Utilities',
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
     ],
     test_suite='tests',
     entry_points={
